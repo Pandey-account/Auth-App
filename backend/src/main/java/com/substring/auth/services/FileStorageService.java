77a9d8f90b0ec34cef58e0d6d16ee3084554ddb3
Nitesh <101654614+Pandey-account@users.noreply.github.com>
2026-08-18 17:44:49 +0530

    Update FileStorageService.java

--- backend/src/main/java/com/substring/auth/services/FileStorageService.java
+++ backend/src/main/java/com/substring/auth/services/FileStorageService.java
@@ -2,7 +2,6 @@ package com.substring.auth.services;
 
 import java.io.IOException;
 
-import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,6 +12,7 @@ public interface FileStorageService {
 
     String deleteFile(String fileName);
 
-    ResponseEntity<Resource> getProfilePicture(Authentication authentication)
-            throws IOException;
+    ResponseEntity<?> getProfilePicture(
+            Authentication authentication
+    ) throws IOException;
 }
